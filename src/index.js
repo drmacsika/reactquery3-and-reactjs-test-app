f8dadeb720570bf4e6a2aca725fbe534ef09b997
Archangel Macsika <drmacsika@gmail.com>
2021-08-11 13:25:53 +0100

    Base Commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const client = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <App />
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
