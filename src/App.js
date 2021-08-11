0a03dcf868b8680467fb16d5c84a5ab7ae7320b8
Archangel Macsika <drmacsika@gmail.com>
2021-08-11 14:07:51 +0100

    React Query states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useQuery } from "react-query";
+import React, { useEffect, useState } from "react";
+
+
+
+function Button() {
+  const {data, error} = useQuery("hello-world", () => {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(Math.random()), 1000)
+    })
+  })
+  console.log({data, error})
+  return <button>I am a button {data}.</button>
+}
 
 function App() {
-  useQuery("");
+  const [visible, setVisible] = useState(true)
+
+  function toggleButton() {
+    setVisible(visible => !visible)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
+      {visible && <Button />}
+      <button onClick={toggleButton}>Toggle</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
